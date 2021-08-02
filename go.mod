ab856c0151d35fd6cf9b9d4652b6f7069d64a2bf
christian <christian.avz@gmail.com>
2021-08-01 23:22:42 -0400

    prueba external sctruct

---
+++ go.mod
@@ -0,0 +1,5 @@
+module prueba1
+
+go 1.16
+
+require github.com/cvasquezz/readEnvGO v0.0.0-20210802031008-894620624d4c // indirect
--- main.go
+++ main.go
@@ -2,8 +2,29 @@ package main
 
 import (
 	"fmt"
+
+	external "github.com/cvasquezz/readEnvGO"
 )
+
+var wea = external.Wea{}
+
+type User struct {
+	external.User
+	Apellido string
+}
 
 func main() {
 	fmt.Printf("Hola Mundo \n")
+	wea.Default = false
+	user := User{}
+	user.Nombre = "pepe"
+	user.Apellido = "perez"
+	wea.User = user
+
+	user2 := external.User{}
+	user2.Nombre = "Juan"
+
+	wea.ReadEnv(&user2)
+	fmt.Print("Fin")
+
 }
